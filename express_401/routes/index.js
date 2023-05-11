75af8a9ab0527e57b932636b82d595d56d27dcb8
Faisal ahmed <faisal2410@yahoo.com>
2023-05-11 17:34:25 +0600

    updated

--- express_401/routes/index.js
+++ express_401/routes/index.js
@@ -1,10 +1,35 @@
 const express = require('express');
 let router = express.Router();
-
+const movies = require("../data/movies");
+// console.log(movies);
 router.get("/", (req, res, next) => {
     res.json({
         title:"Express"
     })
+})
+
+router.get("/most_popular", (req, res, next) => { 
+    let page = req.query.page;
+    if (page == undefined) {
+        page = 1;
+    }
+   
+        let results = movies.filter(movie => {
+            return movie.most_popular;
+        })
+
+        const indexToStart = (page - 1) * 20;
+        results = results.slice(indexToStart, indexToStart + 19);
+        // console.log(results.length);
+
+        res.json({
+            page,            
+            results
+        });
+    
+   
+
+
 })
 
 module.exports = router;
--- express_401/server.js
+++ express_401/server.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
+require('dotenv').config()
 const indexRouter = require("./routes/index");
 const movieRouter = require("./routes/movie");
 const searchRouter = require("./routes/search");
 
+const port = process.env.PORT||8000;
+// console.log(port);
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
+
+app.use((req, res, next) => {
+    if (req.query.api_key != "12345") {
+        res.status(401);
+        res.json({
+            msg:"Invalid API key"
+        })
+    } else {        
+        next();
+    }
+})
 
 app.use("/", indexRouter);
 app.use("/movie", movieRouter);
@@ -20,6 +34,6 @@ app.use("/search", searchRouter);
 
 
 
-app.listen(8000, () => {
+app.listen(port, () => {
     console.log("The server is running successfully");
 })
